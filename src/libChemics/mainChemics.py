7c4dd681b3631dcd697404fd18263cff6041345d
Frodo81 <bernd@ferienhof-wunder.de>
2011-09-28 05:43:09 +0200

    adding the common form of the nernest quotation und a class for the constants

--- src/libChemics/mainChemics.py
+++ src/libChemics/mainChemics.py
@@ -4,7 +4,16 @@ Created on 25.09.2011
 @author: bernd
 '''
 
-from math import pow
+from math import pow, log10
+
+class Constants(object):
+    def __init__(self, R_gas=8.3144621, faraday=96485.3365):
+        '''
+        Constructor
+        '''
+        self.R_gas = R_gas # J/(mol K)
+        self.faraday = faraday # (C / mol)
+
 
 class Peukert(object):
     '''
@@ -22,5 +31,13 @@ class Peukert(object):
         time = (self.Capacity / pow(self.I_discharge,self.Peukertnumber)) * pow(1,self.Peukertnumber-1)       
         return (time, time*self.I_discharge)
          
-    
+class Nernest(object):    
+    '''
+    classdocs
+    '''
+    def __init__(self):
+        self.K = Constants()
+        
+    def commonForm(self, potenial, Q_reaction, n_elektrons=1, temp=25):            
+        return potenial - (2.303 * self.K.R_gas * temp / n_elektrons * log10(Q_reaction))    
         
